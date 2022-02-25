24c969497b086be1b8d082203a4490c028e7b7f2
devgenix <emmanueln644@gmail.com>
2022-02-25 14:09:19 +0100

    Update: trying to fix form submission

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,63 +1,66 @@
-const Form = document.querySelector('#formdata');
+const Formm = document.querySelector('#formdata');
 const FormButton = document.querySelector('#submit');
 const Success = document.querySelector('#successtext');
 const error = document.querySelector('#errortext');
 let email = document.getElementById("inputfield");
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-Form.addEventListener('click', e => {
-    e.preventDefault();
+if (Formm) {
+  Formm.addEventListener('submit', e => {
+      e.preventDefault();
+      processForm(Formm);
+  })
+
+  const processForm = formm => {
+
     Success.style.display = 'none';
     error.style.display = 'none';
-    processForm(Form);
 
     if (email.value.length == 0) {
       error.innerHTML = "Please enter an email address";
       error.style.display = 'block';
-
+  
       setTimeout(() => {
         error.style.display = 'none';
       }, 3000);
     } else {
-
+  
       if(email.value.match(mailformat)) {
         FormButton.innerHTML = "";
         FormButton.disabled = true;
         const newSpan = document.createElement('div');
         newSpan.classList.add('loader');
         FormButton.appendChild(newSpan);
-
-
-        const processForm = form => {
-          const data = new FormData(form)
-          data.append('form-name', 'Subscribers');
-          fetch('/', {
-            method: 'POST',
-            body: data,
-          })
-            .then(() => {
-                Success.style.display = 'block';
-                newSpan.classList.remove('loader');
-                FormButton.disabled = false;
-                FormButton.innerHTML = "Subscribe";
-                console.log("Form successfully submitted");
-
-                setTimeout(() => {
-                  Success.style.display = 'none';
-                }, 3000);
-            })
-            .catch((error) => {
-              // alert(error);
-
-              error.innerHTML = error;
-              error.style.display = 'block';
-
+  
+        const data = new FormData(formm)
+        data.append('form-name', 'Subscribers');
+        fetch('/', {
+          method: 'POST',
+          body: data,
+        })
+          .then(() => {
+              Success.style.display = 'block';
+              newSpan.classList.remove('loader');
+              FormButton.disabled = false;
+              FormButton.innerHTML = "Subscribe";
+              console.log("Form successfully submitted");
+    
               setTimeout(() => {
-                error.style.display = 'none';
-                email.innerHTML = "";
+                Success.style.display = 'none';
               }, 3000);
-            });
-        }
+          })
+          .catch((error) => {
+            // alert(error);
+    
+            error.innerHTML = error;
+            error.style.display = 'block';
+    
+            setTimeout(() => {
+              error.style.display = 'none';
+              email.innerHTML = "";
+            }, 3000);
+          });
+
 
         // setTimeout(() => {
         //     Success.style.display = 'block';
@@ -65,14 +68,15 @@ Form.addEventListener('click', e => {
         //     FormButton.disabled = false;
         //     FormButton.innerHTML = "Subscribe";
         //   }, 3000);
-
+  
         } else {
           error.innerHTML = "Please enter a valid email address";
           error.style.display = 'block';
-
+  
           setTimeout(() => {
             error.style.display = 'none';
           }, 3000);
         };
     };
-});
+  }
+};
