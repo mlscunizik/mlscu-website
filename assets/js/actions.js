667145d8bb67f6b5364d99d7d8926d777302cfe2
devgenix <emmanueln644@gmail.com>
2022-02-25 12:34:52 +0100

    Update: trying to fix form submission

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -26,7 +26,7 @@ FormButton.addEventListener('click', e => {
         FormButton.appendChild(newSpan);
 
 
-        let myForm = document.getElementById("form");
+        let myForm = document.getElementById("formdata");
         let formData = new FormData(myForm);
         fetch("/", {
             method: "POST",
